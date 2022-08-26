1ae8920f1c2507d5292072af4e8c490c1959b8d3
Ahmad Bakare <65125857+adelekeahmad@users.noreply.github.com>
2022-08-26 01:51:45 -0700

    Add project files.

---
+++ Constructors/Laptops.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    class Laptops
+    {
+        public string brand;
+        public string processor;
+        public string storageCapacity;
+        public string model;
+        public double screenSize;
+        public string RAM;
+
+        public Laptops( string name) 
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
---
+++ Constructors/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Constructors;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Laptops laptop1 = new Laptops("Folio");
+        laptop1.brand = "Hp";
+        laptop1.model = "folio 9480";
+        laptop1.processor = "intel core-i 5";
+        laptop1.storageCapacity = "500GB";
+        laptop1.RAM = "16GB";
+
+        Laptops laptop2 = new Laptops("Dell");
+        laptop2.brand = "Dell";
+        laptop2.model = "Latitude 7440";
+        laptop2.processor = "intel core-i 7";
+        laptop2.storageCapacity = "500GB";
+        laptop2.RAM = "32GB";
+
+        Laptops laptop3 = new Laptops("MacBook");
+        laptop3.brand = "MacBook Pro";
+        laptop3.model = "2021";
+        laptop3.processor = "intel core-i 7";
+        laptop3.storageCapacity = "500GB";
+        laptop3.RAM = "64GB";
+
+                Console.ReadLine();
+    }
+}
